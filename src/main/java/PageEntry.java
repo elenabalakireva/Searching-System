6180301c308d858e18875bb941fbcbcd8affdba5
elenabalakireva <100164525+elenabalakireva@users.noreply.github.com>
2022-05-20 16:16:48 +0300

    Update PageEntry.java

--- src/main/java/PageEntry.java
+++ src/main/java/PageEntry.java
@@ -1,7 +1,68 @@
-public class PageEntry implements Comparable<PageEntry> {
-    private final String pdfName;
-    private final int page;
-    private final int count;
+import java.util.Objects;
 
-    // ???
+public class PageEntry implements Comparable<PageEntry> {
+    private String pdfName;
+    private int page;
+    private int count;
+
+    public PageEntry() {
+    }
+
+    public PageEntry(String pdfName, int page, int count) {
+        this.pdfName = pdfName;
+        this.page = page;
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return "PageEntry{" +
+                "pdfName='" + pdfName + '\'' +
+                ", page=" + page +
+                ", count=" + count +
+                '}';
+    }
+
+    @Override
+    public int compareTo(PageEntry o) {
+        if (count < o.count) {
+            return -1;
+        } else return 1;
+
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getPdfName() {
+        return pdfName;
+    }
+
+    public void setPdfName(String pdfName) {
+        this.pdfName = pdfName;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        PageEntry o = (PageEntry) obj;
+        return pdfName.equals(o.pdfName) && page == o.page;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pdfName, page);
+    }
 }
