dbbb6a0d50afbf12f0b15349e56b73b0392a2355
elenabalakireva <100164525+elenabalakireva@users.noreply.github.com>
2022-05-20 16:16:27 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,42 @@
-import java.io.File;
-import java.util.Arrays;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
-        System.out.println(engine.search("бизнес"));
+    static final String PATH = "C:\\Users\\Admin\\IdeaProjects\\Searching\\pdfs";
+    static final int port = 8989;
 
-        // здесь создайте сервер, который отвечал бы на нужные запросы
-        // слушать он должен порт 8989
-        // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
+    public static void main(String[] args) throws Exception {
+
+        BooleanSearchEngine booleanSearchEngine = new BooleanSearchEngine(new HashMap<>());
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            out.println("Write a word");
+            final String word = in.readLine();
+
+            List<PageEntry> sortedList = booleanSearchEngine.search(word);
+            out.println(stringToJson(sortedList));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String stringToJson(List<PageEntry> list) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(list);
+        return json;
+
     }
 }
